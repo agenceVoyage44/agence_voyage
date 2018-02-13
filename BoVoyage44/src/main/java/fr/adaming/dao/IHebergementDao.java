b4b751ea5507c38f6a7c9d82d0e4443b7829d651
romai <romai@192.168.1.128>
2018-02-13 12:21:11 +0100

    rom: hebergement

--- BoVoyage44/src/main/java/fr/adaming/dao/IHebergementDao.java
+++ BoVoyage44/src/main/java/fr/adaming/dao/IHebergementDao.java
@@ -7,13 +7,41 @@ import fr.adaming.model.Hebergement;
 
 public interface IHebergementDao {
 
+	/**
+	 * Méthode permettant d'ajouter un Hebergement
+	 * @param hebergement
+	 * @return retourne un objet de type hebergement
+	 */
 	public Hebergement addHebergement(Hebergement hebergement);
 
+	
+	/**
+	 * Méthode permettant de modifier un hebergement
+	 * @param hebergement
+	 * @return retourne un objet hebergement mis à jour
+	 */
 	public Hebergement updateHebergement(Hebergement hebergement);
 
+	
+	/**
+	 * Méthode permettant de rechercher un hebergement pr son id
+	 * @param idHebergement
+	 * @return l'hebergement recherché par son id
+	 */
 	public Hebergement getHebergementByID(int idHebergement);
 
+	
+	/**
+	 * Méthode permettant de supprimer un hebergement
+	 * @param idHebergement
+	 * @return 0 si la suppression a échoué, 1 si cette dernière est réussi
+	 */
 	public int deleteHebergement(int idHebergement);
 
+	
+	/**
+	 * Méthode pour récupérer toutes les hebergements
+	 * @return retourne une liste des hebergements
+	 */
 	public List<Hebergement> getAllHebergement();
 }
